cc0fd513346ed23a49bccf3a83aaa2c28f2190ba
DavidSarlle <trapeya25@gmail.com>
2022-10-25 18:20:24 +0400

    implementing cryoscope experiment

---
+++ src/qibocal/calibrations/characterization/cryoscope.py
@@ -0,0 +1,288 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from qibolab.platforms.abstract import AbstractPlatform
+from qibolab.pulses import FluxPulse, Pulse, PulseSequence, PulseType, Rectangular
+
+from qibocal import plots
+from qibocal.calibrations.characterization.utils import iq_to_prob
+from qibocal.data import Dataset
+from qibocal.decorators import plot
+
+
+@plot("cryoscope", plots.cryoscope)
+# @plot("cryoscope slider", plots.cryoscope_raw_slider)
+def cryoscope(
+    platform: AbstractPlatform,
+    qubit: int,
+    flux_pulse_duration_start,
+    flux_pulse_duration_end,
+    flux_pulse_duration_step,
+    flux_pulse_amplitude_start,
+    flux_pulse_amplitude_end,
+    flux_pulse_amplitude_step,
+    points=10,
+):
+    # 1) from state |0> apply Ry(pi/2) to state |+>,
+    # 2) apply a flux pulse of variable duration,
+    # 3) measure in the X and Y axis
+    #   MX = Ry(pi/2) - (flux)(t) - Ry(pi/2)  - MZ
+    #   MY = Ry(pi/2) - (flux)(t) - Rx(-pi/2) - MZ
+    #   MZ = Ry(pi/2) - (flux)(t) - wait      - MZ
+    # The flux pulse detunes the qubit and results in a rotation around the Z axis = atan(MY/MX)
+
+    platform.reload_settings()
+    mean_gnd = complex(
+        platform.characterization["single_qubit"][qubit]["mean_gnd_states"]
+    )
+    mean_exc = complex(
+        platform.characterization["single_qubit"][qubit]["mean_exc_states"]
+    )
+
+    # start at |+> by rotating Ry(pi/2)
+    initial_RY90_pulse = platform.create_RX90_pulse(
+        qubit, start=0, relative_phase=np.pi / 2
+    )
+    # apply a detuning flux pulse
+    flux_pulse = FluxPulse(
+        start=initial_RY90_pulse.se_finish + 8,
+        duration=flux_pulse_duration_start,  # sweep to produce oscillations [300 to 400ns] in steps od 1ns? or 4?
+        amplitude=flux_pulse_amplitude_start,  # fix for each run
+        shape=Rectangular(),  # should be rectangular, but it gets distorted
+        relative_phase=0,
+        channel=platform.qubit_channel_map[qubit][2],
+        qubit=qubit,
+    )
+
+    # rotate around the X asis Rx(-pi/2) to meassure Y component
+    RX90_pulse = platform.create_RX90_pulse(
+        qubit, start=flux_pulse.se_finish + 8, relative_phase=np.pi
+    )
+
+    # rotate around the Y asis Ry(pi/2) to meassure X component
+    RY90_pulse = platform.create_RX90_pulse(
+        qubit, start=flux_pulse.se_finish + 8, relative_phase=np.pi / 2
+    )
+
+    # add ro pulse at the end of each sequence
+    MZ_ro_pulse = platform.create_qubit_readout_pulse(qubit, start=RX90_pulse.se_finish)
+
+    # build the sequences
+    MX_seq = initial_RY90_pulse + flux_pulse + RY90_pulse + MZ_ro_pulse
+    MY_seq = initial_RY90_pulse + flux_pulse + RX90_pulse + MZ_ro_pulse
+    MZ_seq = initial_RY90_pulse + flux_pulse + MZ_ro_pulse
+
+    MX_tag = "MX"
+    MY_tag = "MY"
+    MZ_tag = "MZ"
+
+    data = Dataset(
+        name=f"data_q{qubit}",
+        quantities={
+            "flux_pulse_duration": "ns",
+            "flux_pulse_amplitude": "dimensionless",
+            "prob": "dimensionless",
+        },
+        options=["component"],
+    )
+
+    amplitudes = np.arange(
+        flux_pulse_amplitude_start, flux_pulse_amplitude_end, flux_pulse_amplitude_step
+    )
+    # durations_more = np.arange(
+    #     flux_pulse_duration_start,
+    #     int(
+    #         flux_pulse_duration_start
+    #         + (flux_pulse_duration_end - flux_pulse_duration_start)
+    #         / 8
+    #         // flux_pulse_duration_step
+    #         * flux_pulse_duration_step
+    #     ),
+    #     flux_pulse_duration_step,
+    # )
+    # durations_less = np.arange(
+    #     int(
+    #         flux_pulse_duration_start
+    #         + (
+    #             (
+    #                 (flux_pulse_duration_end - flux_pulse_duration_start)
+    #                 / 8
+    #                 // flux_pulse_duration_step
+    #             )
+    #             + 1
+    #         )
+    #         * flux_pulse_duration_step
+    #     ),
+    #     flux_pulse_duration_end,
+    #     flux_pulse_duration_step * 4,
+    # )
+    # durations = np.concatenate((durations_more, durations_less))
+    durations = np.arange(
+        flux_pulse_duration_start, flux_pulse_duration_end, flux_pulse_duration_step
+    )
+    count = 0
+
+    for amplitude in amplitudes:
+        for duration in durations:
+            if count % points == 0:
+                yield data
+            flux_pulse.amplitude = amplitude
+            flux_pulse.duration = duration
+
+            MX_results = platform.execute_pulse_sequence(MX_seq)[MZ_ro_pulse.serial]
+            results = {
+                "MSR[V]": MX_results[0],
+                "i[V]": MX_results[2],
+                "q[V]": MX_results[3],
+                "phase[rad]": MX_results[1],
+                "prob[dimensionless]": iq_to_prob(
+                    MX_results[2], MX_results[3], mean_gnd, mean_exc
+                ),
+                "component": MX_tag,
+                "flux_pulse_duration[ns]": duration,
+                "flux_pulse_amplitude[dimensionless]": amplitude,
+            }
+            data.add(results)
+
+            MY_results = platform.execute_pulse_sequence(MY_seq)[MZ_ro_pulse.serial]
+            results = {
+                "MSR[V]": MY_results[0],
+                "i[V]": MY_results[2],
+                "q[V]": MY_results[3],
+                "phase[rad]": MY_results[1],
+                "prob[dimensionless]": iq_to_prob(
+                    MY_results[2], MY_results[3], mean_gnd, mean_exc
+                ),
+                "component": MY_tag,
+                "flux_pulse_duration[ns]": duration,
+                "flux_pulse_amplitude[dimensionless]": amplitude,
+            }
+            data.add(results)
+
+            MZ_results = platform.execute_pulse_sequence(MZ_seq)[MZ_ro_pulse.serial]
+            results = {
+                "MSR[V]": MZ_results[0],
+                "i[V]": MZ_results[2],
+                "q[V]": MZ_results[3],
+                "phase[rad]": MZ_results[1],
+                "prob[dimensionless]": iq_to_prob(
+                    MZ_results[2], MZ_results[3], mean_gnd, mean_exc
+                ),
+                "component": MZ_tag,
+                "flux_pulse_duration[ns]": duration,
+                "flux_pulse_amplitude[dimensionless]": amplitude,
+            }
+            data.add(results)
+
+            count += 1
+        yield data
+
+
+# @plot("cryoscope", plots.cryoscope_amplitude)
+def cryoscope_amplitude(
+    platform: AbstractPlatform,
+    qubit: int,
+    flux_pulse_amplitude_start,
+    flux_pulse_amplitude_end,
+    flux_pulse_amplitude_step,
+    points=10,
+):
+    # 1) from state |0> apply Ry(pi/2) to state |+>,
+    # 2) apply a flux pulse of variable duration,
+    # 3) measure in the X and Y axis
+    #   MX = Ry(pi/2) - (flux)(t) - Ry(pi/2)  - MZ
+    #   MY = Ry(pi/2) - (flux)(t) - Rx(-pi/2) - MZ
+    #   MZ = Ry(pi/2) - (flux)(t) - wait      - MZ
+    # The flux pulse detunes the qubit and results in a rotation around the Z axis = atan(MY/MX)
+
+    platform.reload_settings()
+
+    # start at |+> by rotating Ry(pi/2)
+    initial_RY90_pulse = platform.create_RX90_pulse(
+        qubit, start=0, relative_phase=np.pi / 2
+    )
+    # apply a detuning flux pulse
+    flux_pulse = FluxPulse(
+        start=initial_RY90_pulse.se_finish,
+        duration=40,  # sweep to produce oscillations [300 to 400ns] in steps od 1ns? or 4?
+        amplitude=flux_pulse_amplitude_start,  # fix for each run
+        shape=Rectangular(),  # should be rectangular, but it gets distorted
+        channel=platform.qubit_channel_map[qubit][2],
+        qubit=qubit,
+    )
+
+    # rotate around the X asis Rx(-pi/2) to meassure Y component
+    RX90_pulse = platform.create_RX90_pulse(
+        qubit, start=flux_pulse.se_finish, relative_phase=np.pi
+    )
+
+    # rotate around the Y asis Ry(pi/2) to meassure X component
+    RY90_pulse = platform.create_RX90_pulse(
+        qubit, start=flux_pulse.se_finish, relative_phase=np.pi / 2
+    )
+
+    # add ro pulse at the end of each sequence
+    MZ_ro_pulse = platform.create_qubit_readout_pulse(qubit, start=RX90_pulse.se_finish)
+
+    # build the sequences
+    MX_seq = initial_RY90_pulse + flux_pulse + RY90_pulse + MZ_ro_pulse
+    MY_seq = initial_RY90_pulse + flux_pulse + RX90_pulse + MZ_ro_pulse
+    MZ_seq = initial_RY90_pulse + flux_pulse + MZ_ro_pulse
+
+    MX_tag = "MX"
+    MY_tag = "MY"
+    MZ_tag = "MZ"
+
+    data = Dataset(
+        name=f"data_q{qubit}",
+        quantities={
+            "flux_pulse_duration": "ns",
+            "flux_pulse_amplitude": "dimensionless",
+        },
+        options=["component"],
+    )
+
+    amplitudes = np.arange(
+        flux_pulse_amplitude_start, flux_pulse_amplitude_end, flux_pulse_amplitude_step
+    )
+    count = 0
+
+    for amplitude in amplitudes:
+        if count % points == 0:
+            yield data
+        flux_pulse.amplitude = amplitude
+
+        MX_results = platform.execute_pulse_sequence(MX_seq)[MZ_ro_pulse.serial]
+        results = {
+            "MSR[V]": MX_results[0],
+            "i[V]": MX_results[2],
+            "q[V]": MX_results[3],
+            "phase[rad]": MX_results[1],
+            "component": MX_tag,
+            "flux_pulse_amplitude[dimensionless]": amplitude,
+        }
+        data.add(results)
+
+        MY_results = platform.execute_pulse_sequence(MY_seq)[MZ_ro_pulse.serial]
+        results = {
+            "MSR[V]": MY_results[0],
+            "i[V]": MY_results[2],
+            "q[V]": MY_results[3],
+            "phase[rad]": MY_results[1],
+            "component": MY_tag,
+            "flux_pulse_amplitude[dimensionless]": amplitude,
+        }
+        data.add(results)
+
+        MZ_results = platform.execute_pulse_sequence(MZ_seq)[MZ_ro_pulse.serial]
+        results = {
+            "MSR[V]": MZ_results[0],
+            "i[V]": MZ_results[2],
+            "q[V]": MZ_results[3],
+            "phase[rad]": MZ_results[1],
+            "component": MZ_tag,
+            "flux_pulse_amplitude[dimensionless]": amplitude,
+        }
+        data.add(results)
+
+        count += 1
+    yield data
---
+++ src/qibocal/plots/cryosocpe.py
@@ -0,0 +1,315 @@
+  # -*- coding: utf-8 -*-
+from cProfile import label
+
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.graph_objects as go
+import scipy
+import scipy.signal as ss
+from plotly.subplots import make_subplots
+from scipy.optimize import curve_fit
+
+from qibocal.data import Data, Dataset
+from qibocal.fitting.utils import cos, exp, flipping, lorenzian, rabi, ramsey
+from scipy.signal import lfilter
+
+
+# For cryoscope
+def cryoscope(folder, routine, qubit, format):
+    data = Dataset.load_data(folder, routine, format, f"data_q{qubit}")
+    import numpy as np
+
+    MX_tag = "MX"
+    MY_tag = "MY"
+    MZ_tag = "MZ"
+
+    # Extracting Data
+    z = data.get_values("prob", "dimensionless")[
+        data.df["component"] == MZ_tag
+    ].to_numpy()
+    x = data.get_values("prob", "dimensionless")[
+        data.df["component"] == MX_tag
+    ].to_numpy()
+    y = data.get_values("prob", "dimensionless")[
+        data.df["component"] == MY_tag
+    ].to_numpy()
+    x = x[: len(z)]
+    y = y[: len(z)]
+
+    amplitude = data.get_values("flux_pulse_amplitude", "dimensionless")
+    duration = data.get_values("flux_pulse_duration", "ns")
+    flux_pulse_duration = duration[data.df["component"] == MZ_tag].to_numpy()
+    flux_pulse_amplitude = amplitude[data.df["component"] == MZ_tag].to_numpy()
+    amplitude_unique = flux_pulse_amplitude[
+        flux_pulse_duration == flux_pulse_duration[0]
+    ]
+    duration_unique = flux_pulse_duration[
+        flux_pulse_amplitude == flux_pulse_amplitude[0]
+    ]
+
+    # Making figure
+    figs = []
+
+    figs = [
+        make_subplots(
+            rows=2,
+            cols=1,
+            horizontal_spacing=0.1,
+            vertical_spacing=0.2,
+            subplot_titles=(
+                "X_raw",
+                "Y_raw",
+            ),
+        ),
+        make_subplots(
+            rows=2,
+            cols=1,
+            horizontal_spacing=0.1,
+            vertical_spacing=0.2,
+            subplot_titles=(
+                "X_norm",
+                "Y_norm",
+            ),
+        ),
+    ]
+
+    figs[0].add_trace(
+        go.Heatmap(
+            x=flux_pulse_duration,
+            y=flux_pulse_amplitude,
+            z=x,
+            colorbar=dict(len=0.46, y=0.75),
+        ),
+        row=1,
+        col=1,
+    )
+
+    figs[0].add_trace(
+        go.Heatmap(
+            x=flux_pulse_duration,
+            y=flux_pulse_amplitude,
+            z=y,
+            colorbar=dict(len=0.46, y=0.25),
+        ),
+        row=2,
+        col=1,
+    )
+    figs[0].update_layout(
+        xaxis_title="Pulse duration (ns)",
+        yaxis_title="Amplitude (a.u.)",
+        xaxis2_title="Pulse duration (ns)",
+        yaxis2_title="Amplitude (a.u.)",
+        title=f"Raw data",
+    )
+
+    # Smoothing and -1/1 center
+    smoothing = False
+    x_norm = np.empty((len(amplitude_unique), len(duration_unique)))
+    y_norm = np.empty((len(amplitude_unique), len(duration_unique)))
+    for i, amp in enumerate(amplitude_unique):
+        if smoothing:
+            x_norm[i, :], y_norm[i, :] = normalize_sincos(
+                x[amplitude == flux_pulse_amplitude[i]],
+                y[amplitude == flux_pulse_amplitude[i]],
+            )  # FIXME: not working yet
+        else:  # To put on one line
+            idx = np.where(flux_pulse_amplitude == amp)
+            if amp == flux_pulse_amplitude[-1]:
+                n = int(np.where(flux_pulse_duration[-1] == duration_unique)[0][0]) + 1
+            else:
+                n = len(duration_unique)
+            x_norm[i, :n] = x[idx]
+            x_norm[i, :n] = x_norm[i, :n] - 0.5
+            x_norm[i, :n] = x_norm[i, :n] / max(abs(x_norm[i, :n]))
+            y_norm[i, :n] = y[idx]
+            y_norm[i, :n] = y_norm[i, :n] - 0.5
+            y_norm[i, :n] = y_norm[i, :n] / max(abs(y_norm[i, :n]))
+
+    figs[1].add_trace(
+        go.Heatmap(
+            x=duration_unique,
+            y=amplitude_unique,
+            z=x_norm,
+            colorbar=dict(len=0.46, y=0.75),
+        ),
+        row=1,
+        col=1,
+    )
+
+    figs[1].add_trace(
+        go.Heatmap(
+            x=duration_unique,
+            y=amplitude_unique,
+            z=y_norm,
+            colorbar=dict(len=0.46, y=0.25),
+        ),
+        row=2,
+        col=1,
+    )
+    figs[1].update_layout(
+        xaxis_title="Pulse duration (ns)",
+        yaxis_title="Amplitude (a.u.)",
+        xaxis2_title="Pulse duration (ns)",
+        yaxis2_title="Amplitude (a.u.)",
+        title=f"Norm data",
+    )
+
+    # Plot the phase between X and Y
+    figs += [go.Figure()]
+    phi = np.arctan2(x_norm, y_norm)
+    figs[2].add_trace(
+        go.Heatmap(
+            x=duration_unique,
+            y=amplitude_unique,
+            z=phi,
+        ),
+    )
+    figs[2].update_layout(
+        xaxis_title="Pulse duration (ns)", yaxis_title="Amplitude (a.u.)", title=f"Phi"
+    )
+
+    # Plot the unwrapped
+    figs += [go.Figure()]
+    phi_unwrap = np.unwrap(
+        phi, axis=0
+    )  # Axis 0 = unwrap along row <==> amplitude / Axis 1 = unwrap along row <==> duration
+    figs[3].add_trace(
+        go.Heatmap(
+            x=duration_unique,
+            y=amplitude_unique,
+            z=phi_unwrap,
+        ),
+    )
+    figs[3].update_layout(
+        xaxis_title="Pulse duration (ns)",
+        yaxis_title="Amplitude (a.u.)",
+        title=f"Phi unwrapped",
+    )
+
+    # Plot frequency shift (z) for amplitude duration = real pulse
+    figs += [go.Figure()]
+
+    # dt = np.diff(duration_unique) * 1e-9
+    # dphi_dt_unwrap = np.diff(phi_unwrap, axis=1) / dt
+    dphi_dt_unwrap = phi_unwrap * 1e9 / duration_unique
+    detuning = dphi_dt_unwrap / (2 * np.pi)
+    figs[4].add_trace(
+        go.Heatmap(
+            x=duration_unique,
+            y=amplitude_unique,
+            z=detuning,
+        ),
+    )
+    figs[4].update_layout(
+        xaxis_title="Pulse duration (ns)",
+        yaxis_title="Amplitude (a.u.)",
+        title=f"Detuning (Hz)",
+    )
+
+    # From the Amplitude(Detunning) function, reconstruct the pulse, Amplitude(time) from the measured detunning
+    figs += [
+        go.Figure()
+    ]  # 5 for the stable detuning frequency as a function of amplitude
+    figs += [go.Figure()]  # 6 for the reconstructed pulse
+
+    detuning_median = np.median(detuning, axis=1)  # Stable detuning after a long pulse
+    figs[5].add_trace(
+        go.Scatter(
+            x=amplitude_unique,
+            y=detuning_median,
+        ),
+    )
+    figs[5].update_layout(
+        xaxis_title="Amplitude (a.u.)",
+        yaxis_title="Detuning (Hz)",
+        title=f"Detuning as a function of amplitude",
+    )
+
+    pulses = np.empty((len(amplitude_unique), len(duration_unique)))
+    a_b_fits = np.empty((len(amplitude_unique), 4))
+    for i, amp in enumerate(amplitude_unique):
+        awg_reconstructed_pulse = np.interp(
+            detuning[i, :],  # np.append(detuning[i, :], [detuning_median[i]]),
+            detuning_median,
+            amplitude_unique,
+        )
+        figs[6].add_trace(
+            go.Scatter(
+                visible=False,
+                x=duration_unique,
+                y=awg_reconstructed_pulse,
+                name=f"A = {amp}",
+            ),
+        )
+
+        b_fit, a_fit = cryoscope_fit(
+            duration_unique,
+            amp * np.ones(len(duration_unique)),
+            awg_reconstructed_pulse,
+        )
+        a_b_fits[i, :] = np.append(a_fit, b_fit)
+        pulses[i, :] = awg_reconstructed_pulse
+
+    figs[6].data[-1].visible = True
+    steps = []
+    for i in range(len(figs[6].data)):
+        step = dict(
+            method="restyle",
+            args=["visible", [False] * len(figs[6].data)],
+            label=str(amplitude_unique[i]),
+        )
+        step["args"][1][i] = True  # Toggle i'th trace to "visible"
+        steps.append(step)
+    sliders = [
+        dict(steps=steps, active=10, currentvalue={"prefix": "Amplitude (a.u.) = "})
+    ]
+    figs[6].update_layout(
+        xaxis_title="Pulse duration (ns)",
+        yaxis_title="Amplitude (a.u.)",
+        title=f"Reconstructed pulse",
+        sliders=sliders,
+    )
+
+    return figs
+
+
+def normalize_sincos(x, y, window_size_frac=61, window_size=None, do_envelope=True):
+
+    if window_size is None:
+        window_size = len(x) // window_size_frac
+
+        # window size for savgol filter must be odd
+        window_size -= (window_size + 1) % 2
+
+    x = ss.savgol_filter(x, window_size, 0, 0)  ## why poly order 0
+    y = ss.savgol_filter(y, window_size, 0, 0)
+
+    return x, y  # this is returning just the noise,
+
+
+def cryoscope_fit(x_data, y_data, awg_reconstructed_pulse):
+    pguess = [1, 0, 1, 0]  # b0  # b1  # a0  # a1
+    try:
+        popt, pcov = curve_fit(
+            lambda t, *p: cryoscope_step_response(t, p, awg_reconstructed_pulse),
+            x_data,
+            y_data,
+            p0=pguess,
+        )
+        b_fit = popt[[0, 1]]
+        a_fit = popt[[2, 3]]
+    except:
+        a_fit = [np.nan, np.nan]
+        b_fit = [np.nan, np.nan]
+
+    return b_fit, a_fit
+
+
+def cryoscope_step_response(t, p, awg_reconstructed_pulse):
+    # https://arxiv.org/pdf/1907.04818.pdf (page 11 - filter formula S22)
+    # p = [b0 = 1−k +k ·α, b1 = −(1−k)·(1−α),a0 = 1 and a1 = −(1−α)]
+    # p = [b0, b1, a0, a1]
+    
+    b_fit = p[:2]
+    a_fit = p[2:]
+    return lfilter(b_fit, a_fit, awg_reconstructed_pulse)
