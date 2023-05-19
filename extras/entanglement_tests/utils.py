f35755479e2317d7bb0cfbb03921397722e8e3a5
igres26 <igres26@gmail.com>
2023-05-19 15:21:40 +0400

    small change utils

--- extras/entanglement_tests/utils.py
+++ extras/entanglement_tests/utils.py
@@ -9,11 +9,11 @@ def calculate_frequencies(result1, result2, result3):
     return {f"{int(v1)}{int(v2)}{int(v3)}": cnt for (v1, v2, v3), cnt in zip(values, counts)}
 
 
-def calculate_probabilities(result1, result2):
+def calculate_probabilities(result1, result2, result3):
     """Calculates two-qubit outcome probabilities from individual shots."""
-    shots = np.stack([result1.shots, result2.shots]).T
+    shots = np.stack([result1.shots, result2.shots, result3.shots]).T
     values, counts = np.unique(shots, axis=0, return_counts=True)
     nshots = np.sum(counts)
     return {
-        f"{int(v1)}{int(v2)}": cnt / nshots for (v1, v2), cnt in zip(values, counts)
+        f"{int(v1)}{int(v2)}{int(v3)}": cnt / nshots for (v1, v2, v3), cnt in zip(values, counts)
     }
